384193d7cedaabcdf59299dec31cfe5aa0fe71fb
ktaeu <hesshes@naver.com>
2025-04-20 14:05:48 +0900

    a

---
+++ src/main/java/com/keezna/csl/report/KeeznaCslReportApplication.java
@@ -0,0 +1,12 @@
+package com.keezna.csl.report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeeznaCslReportApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KeeznaCslReportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/keezna/csl/report/KeeznaCslReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keezna.csl.report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeeznaCslReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
